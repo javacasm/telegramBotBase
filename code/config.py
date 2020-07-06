b53890fc8a8770f3dc7f94bb29e197726a1fd392
Javacasm <javacasm@gmail.com>
2020-07-06 09:55:17 +0200

    add

---
+++ code/config.py
@@ -0,0 +1,12 @@
+# Configuration file
+
+# Telegram Bot Authorization Token
+
+TELEGRAM_API_TOKEN = 'YOUR TELEGRAM API KEY'
+
+v = '0.8'
+
+# chat_id del Admin
+ADMIN_USER = 123123123 
+
+ALLOWED_USERS = [ADMIN_USER]
